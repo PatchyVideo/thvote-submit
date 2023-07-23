a6b8d6aa96fc0b84446fd6322ec8595fe34c21e8
zyddnys <zyddnys@outlook.com>
2023-07-22 20:39:29 -0400

    name->id

--- src/common.rs
+++ src/common.rs
@@ -1,18 +1,7 @@
 
-use actix_web::{HttpResponse, web};
-use actix_web::{http::StatusCode, ResponseError};
 use chrono::Utc;
 use pvrustlib::ServiceError;
 use redis::AsyncCommands;
-use std::fmt::{Display, Formatter, Result as FmtResult};
-use serde_json::{json, to_string_pretty};
-use serde::{Serialize, Deserialize};
-use thiserror::Error;
-
-pub const VOTE_YEAR: i32 = 2021;
-pub const MAX_ATTEMPTS: i32 = 10;
-
-use mongodb::{Collection, Database};
 
 pub static SERVICE_NAME: &'static str = "submit-handler";
 
